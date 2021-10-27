58fc101f028e8c5b7124c1f84a1ae9aa42c4fbeb
Conner Cochrane <cccochrane1@knights.ucf.edu>
2021-10-27 14:19:04 -0400

    setting up the file for indexedDB to establish connection and working

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,3 +1,27 @@
 let db;
 
 const request = indexedDB.open('budget_tracker', 1);
+
+request.onupgradeneeded = function(event) {
+    const db = event.target.result
+    db.createObjectStore('budget', { autoIncrement: true});
+}
+
+request.onsuccess = function(event) {
+    db = event.target.result
+    if (navigator.online) {
+        uploadBudget();
+    }
+};
+
+request.onerror = function(event){
+    console.log(event.target.errorCode);
+}
+
+function saveRecord(record) {
+    const transaction = db.transaction(['new_budget'], 'readwrite');
+
+    const budgetTrackerObjectStore = transaction.objectStore('new_budget');
+
+    budgetTrackerObjectStore.add(record);
+}
